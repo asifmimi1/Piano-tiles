22d2a63e7fd17fbc93fe0df7407cbd9ff0c014f3
ASIF MIMI <asif.mimi1@gmail.com>
2020-01-17 17:39:07 +0600

    1. Xylophone 2. Auto Layout 3. Constrains 4. Suffle

--- Piano-tiles/ViewController.swift
+++ Piano-tiles/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    var audioPlayer : AVAudioPlayer!
+    var soundArray = [ "a-3", "f-3", "f-4", "f-5", "f5", "g-4", "g3", "g4", "g5", "c5" ]
+    var audioNumber = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func playButtons(_ sender: UIButton) {
+       audioNumber = soundArray[sender.tag - 1]
+       audioPlayback()
+    }
+    
+    
+    
+    func audioPlayback() {
+        let soundUrl = Bundle.main.url(forResource: audioNumber, withExtension: "mp3")
+        
+        do{
+             audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        }
+        catch{
+            print(error)
+        }
+        audioPlayer.play()
+    }
+    
 }
 
